76f2381f1147eb329225516e89861628b3dfc4d8
Kritsanaphong Tepwerakul <kritsanaphong.t@transcode.co.th>
2024-11-17 20:27:28 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        heboo: ['Heebo', 'sans-serif'],
+        inter: ['Inter', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
